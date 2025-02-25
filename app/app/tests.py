1fb55ab1561fb4c53c3684f2c027a47c64510bd3
jvcarpena <jvcarpena2@gmail.com>
2025-02-25 17:18:55 +0800

    Added the test of the database connection

--- app/app/tests.py
+++ app/app/tests.py
@@ -16,7 +16,7 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
 
